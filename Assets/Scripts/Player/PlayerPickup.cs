90e8ca5d946be4633301256c9ed4a66e6622a561
Guilherme Vogt <vogt.guilherme@gmail.com>
2020-03-04 10:27:29 -0300

    Coletando item

---
+++ Assets/Scripts/Player/PlayerPickup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+    public Transform itemPosition;
+
+    private bool isCarrying = false;
+    [SerializeField]
+    private GameObject collectable;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Collect();
+        }
+    }
+
+    private void Collect()
+    {
+        if(collectable != null)
+        {
+            collectable.transform.parent = itemPosition;
+            collectable.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Collectable")
+        {
+            collectable = other.gameObject;
+        }
+    }
+}
