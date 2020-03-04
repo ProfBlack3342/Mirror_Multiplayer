d2c3d5ac3d3d488d218f19a9ba2418792c61e115
Eduardo Moreira <dudu.pm@hotmail.com>
2020-03-04 09:53:41 -0300

    Coletavel

---
+++ Assets/Scripts/PlayerPickup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+    public GameObject sphere;
+    private bool pickedup;
+
+    private void Awake()
+    {
+        pickedup = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Sphere")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                if(!pickedup)
+                {
+                    sphere = other.gameObject;
+                    pickedup = true;
+                }
+                else
+                {
+                    pickedup = false;
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(pickedup)
+        {
+            sphere.transform.position = gameObject.transform.position;
+        }
+    }
+}
