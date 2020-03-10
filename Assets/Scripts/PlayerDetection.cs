ba4a353ede86c9ddf02264114a6cb9ab00b4e031
Eduardo Moreira <dudu.pm@hotmail.com>
2020-03-10 10:19:46 -0300

    Script

---
+++ Assets/Scripts/PlayerDetection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerDetection : NetworkBehaviour
+{
+    private NetworkIdentity id;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!id)
+        {
+            Debug.Log("Colidiu com " + other.gameObject.name);
+        }
+        else
+        {
+            Debug.Log("Colidiu, mas não com outro player");
+        }
+    }
+
+}
